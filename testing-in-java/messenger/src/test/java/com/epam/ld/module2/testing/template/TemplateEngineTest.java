5f0c72c4f6d5d867d04216914ace31a1a5f8cb43
Dzmitry Luzko <luzdim45@gmail.com>
2022-11-24 20:51:20 +0300

    Add generate message test

--- testing-in-java/messenger/src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
+++ testing-in-java/messenger/src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -1,5 +1,17 @@
 package com.epam.ld.module2.testing.template;
 
-public class TemplateEngineTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
+import com.epam.ld.module2.testing.Client;
+
+public class TemplateEngineTest {
+    @Test
+    public void generateMessageFromPlainText() {
+        String expected = "test";
+        TemplateEngine templateEngine = new TemplateEngine();
+        String actual = templateEngine.generateMessage(new Template(), new Client());
+        assertEquals(expected, actual);
+    }
 }
