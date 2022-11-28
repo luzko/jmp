9a314588210e5ed1e7c44a2cb50dd681a8bdfc51
Dzmitry Luzko <luzdim45@gmail.com>
2022-11-28 13:10:07 +0300

    Add some test for mode helper

--- testing-in-java/messenger/src/test/java/com/epam/ld/module2/testing/helper/ModeHelperTest.java
+++ testing-in-java/messenger/src/test/java/com/epam/ld/module2/testing/helper/ModeHelperTest.java
@@ -1,25 +1,33 @@
 package com.epam.ld.module2.testing.helper;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 class ModeHelperTest {
     @Test
     public void consoleModeTest() {
-        String[] args = new String[0];
-        Mode actual = ModeHelper.getMode(args);
+        Mode actual = ModeHelper.getMode(new String[0]);
         assertEquals(Mode.CONSOLE, actual);
     }
 
     @Test
     public void fileModeTest() {
-        String[] args = {
-                "template", "template.txt",
-                "parameters", "parameters.txt",
-                "output", "output.txt",
-        };
-        Mode actual = ModeHelper.getMode(args);
+        Mode actual = ModeHelper.getMode(
+                new String[] {"template", "template.txt", "parameters", "parameters.txt", "output", "output.txt"});
         assertEquals(Mode.FILE, actual);
+    }
+
+    @Test
+    public void incorrectCountParameterArgumentExceptionTest() {
+        assertThrows(ParameterArgumentException.class,
+                () -> ModeHelper.getMode(new String[] {"someParam", "some value"}));
+    }
+
+    @Test
+    public void incorrectParameterArgumentExceptionTest() {
+        assertThrows(ParameterArgumentException.class,
+                () -> ModeHelper.getMode(new String[] {"param1", "value1", "param2", "value2", "param3", "value3"}));
     }
 }
