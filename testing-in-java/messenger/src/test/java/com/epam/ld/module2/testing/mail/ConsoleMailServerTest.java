e16671879c262cb6ffb79dfe1f9bfefa5129f67b
Dzmitry Luzko <luzdim45@gmail.com>
2022-11-28 14:32:04 +0300

    Add implementation for mail server

--- testing-in-java/messenger/src/test/java/com/epam/ld/module2/testing/mail/ConsoleMailServerTest.java
+++ testing-in-java/messenger/src/test/java/com/epam/ld/module2/testing/mail/ConsoleMailServerTest.java
@@ -1,13 +1,11 @@
 package com.epam.ld.module2.testing.mail;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 import java.io.PrintStream;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
 
 class ConsoleMailServerTest {
     @Test
@@ -16,10 +14,8 @@ class ConsoleMailServerTest {
         String addresses = "some@example.com";
         String messageText = "text";
         PrintStream stream = mock(PrintStream.class);
-        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
         System.setOut(stream);
         mailServer.send(addresses, messageText);
-        verify(stream).println(captor.capture());
-        assertEquals(captor.getValue(), "some@example.com text");
+        verify(stream).format("%s%n%s", "some@example.com", "text");
     }
 }
