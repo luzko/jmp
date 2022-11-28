4e9d68683a5f012a4ce2d203c3d30939a6365250
Dzmitry Luzko <luzdim45@gmail.com>
2022-11-28 11:55:45 +0300

    Add some generate message tests

--- testing-in-java/messenger/src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
+++ testing-in-java/messenger/src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -58,7 +58,7 @@ public class TemplateEngineTest {
     @Test
     public void generateMessageWithoutParameterTest() {
         String text = "Dear #{user}, the #{date} is your day!";
-        String addresses = "user1@example.com";
+        String addresses = "user1@gmail.com";
         String user = "Dzmitry";
         Map<String, String> parameters = new HashMap<>();
         parameters.put("user", user);
@@ -66,5 +66,36 @@ public class TemplateEngineTest {
         Client client = new Client(addresses, parameters);
         TemplateEngine templateEngine = new TemplateEngine();
         assertThrows(InvalidDataException.class, () -> templateEngine.generateMessage(template, client));
+    }
+
+    @Test
+    public void generateMessageWithExtraParameterTest() {
+        String text = "Dear #{user}, the #{date} is your day!";
+        String expected = "Dear Dzmitry, the 2022-11-11 is your day!";
+        String addresses = "user1@gmail.com";
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("user", "Dzmitry");
+        parameters.put("date", "2022-11-11");
+        parameters.put("extra1", "extra1");
+        Template template = new Template(text);
+        Client client = new Client(addresses, parameters);
+        TemplateEngine templateEngine = new TemplateEngine();
+        String actual = templateEngine.generateMessage(template, client);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void generateMessageWithSameParameterTest() {
+        String text = "Dear #{user}, the #{date} is your day! Your name is #{user}. Today is #{date}";
+        String expected = "Dear Dzmitry, the 2022-11-11 is your day! Your name is Dzmitry. Today is 2022-11-11";
+        String addresses = "user1@gmail.com";
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("user", "Dzmitry");
+        parameters.put("date", "2022-11-11");
+        Template template = new Template(text);
+        Client client = new Client(addresses, parameters);
+        TemplateEngine templateEngine = new TemplateEngine();
+        String actual = templateEngine.generateMessage(template, client);
+        assertEquals(expected, actual);
     }
 }
