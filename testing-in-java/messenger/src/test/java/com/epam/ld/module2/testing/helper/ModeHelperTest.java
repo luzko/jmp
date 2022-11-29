668abcc33dacc92215fb0942d14c33c0187582db
Dzmitry Luzko <luzdim45@gmail.com>
2022-11-29 21:21:23 +0300

    Add parameterized tests

--- testing-in-java/messenger/src/test/java/com/epam/ld/module2/testing/helper/ModeHelperTest.java
+++ testing-in-java/messenger/src/test/java/com/epam/ld/module2/testing/helper/ModeHelperTest.java
@@ -3,7 +3,12 @@ package com.epam.ld.module2.testing.helper;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import com.epam.ld.module2.testing.exception.ParameterArgumentException;
 
@@ -23,15 +28,30 @@ class ModeHelperTest {
         assertEquals(Mode.FILE, actual);
     }
 
-    @Test
-    public void incorrectCountParameterArgumentExceptionTest() {
-        assertThrows(ParameterArgumentException.class,
-                () -> ModeHelper.getMode(new String[] {"someParam", "some value"}));
+    @ParameterizedTest
+    @MethodSource("wrongArgumentsNumber")
+    public void incorrectCountParameterArgumentExceptionTest(String[] args) {
+        assertThrows(ParameterArgumentException.class, () -> ModeHelper.getMode(args));
     }
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("wrongArgumentsName")
     public void incorrectParameterArgumentExceptionTest() {
         assertThrows(ParameterArgumentException.class,
                 () -> ModeHelper.getMode(new String[] {"param1", "value1", "param2", "value2", "param3", "value3"}));
+    }
+
+    private static Stream<Arguments> wrongArgumentsNumber() {
+        return Stream.of(
+                Arguments.of((Object) new String[] {"param", "value"}),
+                Arguments.of((Object) new String[] {"some", "param", "value"})
+        );
+    }
+
+    private static Stream<Arguments> wrongArgumentsName() {
+        return Stream.of(
+                Arguments.of((Object) new String[] {"template", "path1", "parameters", "path2", "output", "path3"}),
+                Arguments.of((Object) new String[] {"template", "path4", "parameters", "path5", "output", "path6"})
+        );
     }
 }
